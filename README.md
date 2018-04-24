f71930128727351a6e982c077ea65c70b1d809c6
jmbeach <jaredbeachdesign@gmail.com>
2018-04-24 08:29:26 -0500

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+kep-simgen
+========
+
+CLI tool for converting Kepware project files so that they use simulation
+devices instead of real device types.
---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Setup kep-simgen"""
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+HERE = path.dirname(path.realpath(__file__))
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setup(
+    name='kep-simgen',
+    version='0.0.1',
+    description='A Python parser of Kepware projects',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jmbeach/kep-simgen',
+    author='jmbeach',
+    author_email='jaredbeachdesign@gmail.com',
+    keywords='kepware simulation opc',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': ['kep-simgen=kep_simgen.kepware_real_to_simulation:main']
+    }
+)
